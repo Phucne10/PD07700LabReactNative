c1e84afa282c951af2f5d8b0765b6c4da627565c
LabReactNative <phucnxpd07700@fpt.edu.vn>
2024-01-22 19:23:39 +0700

    bo sung

--- src/screens/lab3/index.js
+++ src/screens/lab3/index.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
-import { styles, ColorText } from "./styles";  // import styles và ColorText từ file styles.js
+import { styles, ColorText } from "./styles";  
 
 const ViewComponent = () => {
   const [name, setName] = useState('');
 
   return (
-    <View style={styles.container}>  {/* sửa style thành styles */}
+    <View style={styles.container}> 
       <TextInput
         value={name}
         onChangeText={setName}
         placeholder="Nhap ho ten"
-        style={styles.tiStyle}
+        style={styles.tipStyle}
       />
 
-      <Text style={styles.baseText}>  {/* sửa style thành styles */}
+      <Text style={styles.baseText}> 
         Em vao doi bang {' '}
-        <Text style={[styles.boldText, ColorText('red')]}>Vang doc</Text>  {/* sửa style thành styles */}
+        <Text style={[styles.boldText, ColorText('red')]}>Vang doc</Text> 
       </Text>
     </View>
   );
