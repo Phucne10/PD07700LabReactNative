c63090fcbffeb1cb6bae76a38e5b8b5816579ceb
LabReactNative <phucnxpd07700@fpt.edu.vn>
2024-01-22 20:42:27 +0700

    bổ sung lab4

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -5,42 +5,42 @@ const DATA = [
 {
 Id: '1',
 Title: 'Data Structures',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 {
 Id: '2',
 Title: 'STL',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 {
 Id: '3',
 Title: 'STL',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 {
 Id: '4',
 Title: 'STL',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 {
 Id: '5',
 Title: 'STL',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 {
 Id: '6',
 Title: 'Java',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 {
 Id: '6',
 Title: 'Java',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 {
 Id: '6',
 Title: 'Java',
-imageSource: require('../../asstes/images/anh.png'),
+imageSource: require('../../asstes/images/anh1.png'),
 },
 ];
 
