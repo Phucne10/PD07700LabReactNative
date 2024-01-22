6d3f589f98ec267ce54834e10159466ae217c0ee
LabReactNative <phucnxpd07700@fpt.edu.vn>
2024-01-22 21:12:14 +0700

    Bổ sung

---
+++ src/screens/lab4/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import ListCourse from '../../components/list';
+import {ScrollView} from 'react-native-gesture-handler';
+const Lab4 = () => {
+return (
+<View>
+<ListCourse />
+</View>
+);
+};
+export default React.memo(Lab4);
